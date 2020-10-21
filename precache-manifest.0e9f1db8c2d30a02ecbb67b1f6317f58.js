62472f895286bf8f44a31ce52fbbbab99d7585d0
Prince Raj <Princefebraj08@gmail.com>
2020-10-21 16:17:36 +0530

    Updates

---
+++ precache-manifest.0e9f1db8c2d30a02ecbb67b1f6317f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167a2a99757a91d68959de73964c3db4",
+    "url": "/Contact-List-ReactJS/index.html"
+  },
+  {
+    "revision": "0f67fb983192cfd0b5a7",
+    "url": "/Contact-List-ReactJS/static/js/2.7552d343.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Contact-List-ReactJS/static/js/2.7552d343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdee7166f3408942e5c8",
+    "url": "/Contact-List-ReactJS/static/js/main.2815a4d7.chunk.js"
+  },
+  {
+    "revision": "da95e839a3c77062a4c6",
+    "url": "/Contact-List-ReactJS/static/js/runtime-main.210d9462.js"
+  }
+]);
